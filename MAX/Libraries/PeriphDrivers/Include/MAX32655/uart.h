8b474f9c68faf424be9e5202cbdc6b17de60d0b4
Sihyung Woo <75494566+sihyung-maxim@users.noreply.github.com>
2022-09-28 10:04:07 -0500

    ME17: Resolve linter errors in PeriphDrivers/Include.
    MSDK-Commit: 5bbf5ec6cd80eeb425fabeb028a6e3e230e6dc51

--- MAX/Libraries/PeriphDrivers/Include/MAX32655/uart.h
+++ MAX/Libraries/PeriphDrivers/Include/MAX32655/uart.h
@@ -37,8 +37,8 @@
  *************************************************************************** */
 
 /* Define to prevent redundant inclusion */
-#ifndef _MXC_UART_H_
-#define _MXC_UART_H_
+#ifndef LIBRARIES_PERIPHDRIVERS_INCLUDE_MAX32655_UART_H_
+#define LIBRARIES_PERIPHDRIVERS_INCLUDE_MAX32655_UART_H_
 
 /***** Definitions *****/
 #include "uart_regs.h"
@@ -47,8 +47,6 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-#define UART_EXTCLK_FREQ E_BAD_PARAM
 
 /**
  * @defgroup uart UART
@@ -90,12 +88,15 @@ typedef enum {
 /**
  * @brief      Clock settings */
 typedef enum {
-    /*Only available for UARTS 0-2*/
+    /*For UART3 APB clock source is the 8MHz clock*/
     MXC_UART_APB_CLK = 0,
-    /*Available for all UARTs*/
+    MXC_UART_EXT_CLK = 1,
+    /*IBRO and ERFO clock can only be used for UART 0, 1 & 2*/
     MXC_UART_IBRO_CLK = 2,
+    MXC_UART_ERFO_CLK = 3,
     /*ERTCO clock can only be used for UART3*/
     MXC_UART_ERTCO_CLK = 4,
+    MXC_UART_INRO_CLK = 5
 } mxc_uart_clock_t;
 
 /**
@@ -677,4 +678,4 @@ uint32_t MXC_UART_GetAsyncRXCount(mxc_uart_req_t *req);
 }
 #endif
 
-#endif /* _MXC_UART_H_ */
+#endif // LIBRARIES_PERIPHDRIVERS_INCLUDE_MAX32655_UART_H_
