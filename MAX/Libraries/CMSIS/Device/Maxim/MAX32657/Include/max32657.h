6f544444dc76dfc5cd0e7c23afb4c1a32574c3fb
Tahsin Türker Mutlugün <Tahsin.Mutlugun@analog.com>
2024-08-20 18:16:33 +0300

    feat(PeriphDrivers): Add MAX32657 I3C driver (#1111)
    Signed-off-by: Tahsin Mutlugun <Tahsin.Mutlugun@analog.com>

--- MAX/Libraries/CMSIS/Device/Maxim/MAX32657/Include/max32657.h
+++ MAX/Libraries/CMSIS/Device/Maxim/MAX32657/Include/max32657.h
@@ -634,7 +634,8 @@ We may want to handle GET_IRQ better...
 
 /******************************************************************************/
 /*                                                                        I3C */
-#define MXC_I3C_FIFO_DEPTH (8) // TODO(ME30): Confirm this is correct.
+#define MXC_CFG_I3C_INSTANCES (1)
+#define MXC_I3C_FIFO_DEPTH (8)
 
 /* Non-secure Mapping */
 #define MXC_BASE_I3C_NS ((uint32_t)0x40018000UL)
@@ -651,6 +652,11 @@ We may want to handle GET_IRQ better...
 #define MXC_BASE_I3C MXC_BASE_I3C_NS
 #define MXC_I3C MXC_I3C_NS
 #endif
+
+#define MXC_I3C_GET_BASE(i) ((i) == 0 ? MXC_BASE_I3C : 0)
+#define MXC_I3C_GET_I3C(i) ((i) == 0 ? MXC_I3C : 0)
+#define MXC_I3C_GET_IRQ(i) (IRQn_Type)((i) == 0 ? I3C_IRQn : 0)
+#define MXC_I3C_GET_IDX(p) ((p) == MXC_I3C_NS ? 0 : (p) == MXC_I3C_S ? 0 : -1)
 
 /******************************************************************************/
 /*                                                                        DMA */
