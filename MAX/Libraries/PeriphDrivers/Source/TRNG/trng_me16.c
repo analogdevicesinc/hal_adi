1d2d5447d24a26b6e3e2a771e887b12ad5fbe9f5
Victor Loginov <Victor.Loginov@maximintegrated.com>
2025-01-22 13:52:37 +0300

    Added PAG7920LT sensor

--- MAX/Libraries/PeriphDrivers/Source/TRNG/trng_me16.c
+++ MAX/Libraries/PeriphDrivers/Source/TRNG/trng_me16.c
@@ -38,6 +38,7 @@
 #include "trng_revb.h"
 #include "trng.h"
 
+
 /* ************************************************************************* */
 /* Global Control/Configuration functions                                    */
 /* ************************************************************************* */
@@ -47,9 +48,9 @@
 int MXC_TRNG_Init(void)
 {
     MXC_SYS_ClockEnable(MXC_SYS_PERIPH_CLOCK_TRNG);
-
+    
     MXC_TRNG_RevB_Init();
-
+    
     return E_NO_ERROR;
 }
 
@@ -66,9 +67,9 @@ void MXC_TRNG_DisableInt()
 int MXC_TRNG_Shutdown(void)
 {
     int error = MXC_TRNG_RevB_Shutdown();
-
+    
     MXC_SYS_ClockDisable(MXC_SYS_PERIPH_CLOCK_TRNG);
-
+    
     return error;
 }
 
