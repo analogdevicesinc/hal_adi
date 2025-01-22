6d7e9b60f1791b4fb01331f6b67bc753db487b30
Scheiffler <Jacob.Scheiffler@analog.com>
2025-01-22 13:31:34 +0300

    Fixing GPIO bugs for low-power peripherals.

--- MAX/Libraries/PeriphDrivers/Source/CTB/ctb_reva.c
+++ MAX/Libraries/PeriphDrivers/Source/CTB/ctb_reva.c
@@ -131,7 +131,7 @@ void MXC_CTB_RevA_EnableInt(mxc_ctb_reva_regs_t* ctb_regs)
     ctb_regs->ctrl |= MXC_F_CTB_REVA_CTRL_INTR;
 
 // Enable IRQ
-#ifndef __riscv
+#ifdef CRYPTO_IRQn
     NVIC_EnableIRQ(CRYPTO_IRQn);
 #endif
 }
@@ -139,7 +139,7 @@ void MXC_CTB_RevA_EnableInt(mxc_ctb_reva_regs_t* ctb_regs)
 void MXC_CTB_RevA_DisableInt(mxc_ctb_reva_regs_t* ctb_regs)
 {
 // Disable IRQ
-#ifndef __riscv
+#ifdef CRYPTO_IRQn
     NVIC_DisableIRQ(CRYPTO_IRQn);
 #endif
 
